ec2a396575fc1fbf34d292294b613bf7babc9cad
RokoSi <90521424+RokoSi@users.noreply.github.com>
2024-08-01 22:37:05 +0300

    исправить переменные

--- src/get_url/cli/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["main_menu", "count_user_add_menu", "check_email"]
-
-from .check_email import check_email
-from .count_user_add_menu import count_user_add_menu
-from .menu import main_menu
--- src/get_url/validators/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ["validator_email", "validator_pass"]
-
-from .validator_email import validator_email
-from .validator_password import validator_pass
